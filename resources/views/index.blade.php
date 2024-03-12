41546bc2f16a7360c5b42c8f31240e2e9e09e379
dara3005mj <yhkooh@gmail.com>
2024-03-12 10:50:02 +0700

    hi
    hi

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,5 +9,6 @@
 <body>
     <h1>Hello, World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos repudiandae nisi saepe necessitatibus dolor odit, commodi laborum omnis nesciunt labore rerum autem magni numquam, corrupti quo nobis iste cumque doloribus.</p>
+    <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quis quod est illum ducimus magni consectetur tenetur odio, voluptatibus inventore excepturi labore laborum. Provident consequuntur, tenetur minima sunt illum quasi?</p>
 </body>
 </html>
